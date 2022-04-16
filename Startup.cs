bf9565f68c96b6e6313841f13d3bba39a23c9c80
CivilSalt <chuyetitus@gmail.com>
2022-04-16 23:17:34 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ALMAL_Freelancer.Startup))]
+namespace ALMAL_Freelancer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
